0a44071f399e2fe6d4cfd5231cda654b691f6d91
rishipraneeth-07 <rishipraneethreddy7@gmail.com>
2026-01-09 10:51:48 +0530

    Added /h2-console/** to securityconfig

--- src/main/java/com/todo/securityConfig/SecurityConfig.java
+++ src/main/java/com/todo/securityConfig/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,8 +14,12 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .anyRequest()
-                        .permitAll());
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().permitAll())
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                );
+
         return http.build();
     }
 }
